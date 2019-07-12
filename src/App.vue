9a0428ca7e44c96668890748028804b3d244baac
Jiri Chara <jiri@chara.vip>
2019-07-12 23:13:38 +0200

    Add progressbar

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,41 @@
 
     <router-view/>
 
+    <vue-progress-bar />
+
     <jch-footer />
   </div>
 </template>
 
 <script>
+import { onMounted, onCreated, createComponent } from 'vue-function-api';
+
 import JchNavbar from '@/components/Navbar.vue';
 import JchFooter from '@/components/Footer.vue';
 
-export default {
+export default createComponent({
   components: {
     JchNavbar,
     JchFooter,
   },
-};
+
+  setup(props, { root }) {
+    const { $Progress, $router } = root;
+
+    onMounted(() => $Progress.finish());
+
+    onCreated(()=> {
+      $Progress.start();
+
+      $router.beforeEach((to, from, next) => {
+        $Progress.start()
+        next()
+      });
+
+      $router.afterEach((to, from) => {
+        $Progress.finish();
+      });
+    });
+  },
+});
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Buefy from 'buefy';
 import { plugin } from 'vue-function-api';
+import VueProgressBar from 'vue-progressbar';
 
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -14,8 +15,15 @@ import '@/styles/main.scss';
 
 Vue.config.productionTip = false;
 
+const progressbarOptions = {
+  color: '#0396A6',
+  failedColor: '#874b4b',
+  thickness: '5px',
+};
+
 Vue.use(plugin);
 Vue.use(Buefy);
+Vue.use(VueProgressBar, progressbarOptions);
 
 const app = new Vue({
   router,
