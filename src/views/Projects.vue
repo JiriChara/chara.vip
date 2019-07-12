3d157f941f968976d1772f6a768aada37cd74e2b
Jiri Chara <jiri@chara.vip>
2019-07-12 22:59:19 +0200

    Add projects page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,3 +1,53 @@
 <template>
-  <div>Projects</div>
+  <main>
+    <section class="section">
+      <div class="container content">
+        <h3>
+          <a href="https://github.com/JiriChara/vue-kindergarten">vue-kindergarten</a>
+        </h3>
+        <p>
+          <strong>vue-kindergarten</strong> a plugin for Vue.js that helps you to authorize your
+          components, routes and the rest of your application in a highly modular way.
+        </p>
+
+        <hr />
+
+        <h3>
+          <a href="https://github.com/JiriChara/vuex-crud">vuex-crud</a>
+        </h3>
+        <p>
+          <strong>vuex-crud</strong> is a library for Vuex state management, that helps developers
+          to build CRUD modules easily.
+        </p>
+
+        <hr />
+
+        <h3>
+          <a href="https://github.com/JiriChara/vue-gravatar">vue-gravatar</a>
+        </h3>
+        <p>
+          <strong>vue-gravatar</strong> is a dead-simple component to integrate Gravatar into your
+          Vue.js application.
+        </p>
+
+        <hr />
+
+        <h3>
+          <a href="https://github.com/JiriChara/bavaria-ipsum">bavaria-ipsum</a>
+        </h3>
+        <p>
+          <strong>bavaria-ipsum</strong> is a simple library that generates random Bavarian
+          sentences.
+        </p>
+      </div>
+    </section>
+  </main>
 </template>
+
+<style lang="scss" scoped>
+@import '@/styles/variables.scss';
+
+strong {
+  color: $theme-color-1;
+}
+</style>
